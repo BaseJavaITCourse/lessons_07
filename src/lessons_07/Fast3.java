c9fd57711fd1fa462e9d5398f319ee0f6cbc7e3d
AnnaKovalek <anna.kovalek@gmail.com>
2015-04-10 20:04:17 +0300

    add

---
+++ src/lessons_07/Fast3.java
@@ -0,0 +1,8 @@
+package lessons_07;
+
+public class Fast3 {
+	class InnerClass{
+		
+	}
+
+}
---
+++ src/lessons_07/TestMain.java
@@ -0,0 +1,14 @@
+package lessons_07;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		Fast3 fast3 = new Fast3();
+		
+		//Fast3.InnerClass innerClass = new Fast3.InnerClass();
+		
+	}
+
+}
+
+
